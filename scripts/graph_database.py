6a7aca6b21ecb4b3aded457cb85a9632efac3ea8
Lauren Mutugi <147555663+whoislaurenm@users.noreply.github.com>
2025-06-04 23:06:26 -0400

    Update graph_database.py

--- scripts/graph_database.py
+++ scripts/graph_database.py
@@ -12,16 +12,3 @@ import requests
 
 
 class graph_database:
-    def __init__(self, base_url, repository = "darts_semantic_reasoning"):
-        self.base_url = base_url
-        self.repository = repository
-        self.session = requests.Session()
-
-    def query(self, query):
-        url = f"{self.base_url}/repositories/{self.repository}/statements"
-        response = self.session.post(url, json={"query": query})
-        response.raise_for_status() #throws an exception if there is an HTTP error
-        return response.json()
-
-    def close(self):
-        self.session.close()
